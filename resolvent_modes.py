ad72afc8703eb4d88c23cb69b0431789327dee7e
Thomas Burton <tburton5572@gmail.com>
2021-04-29 12:34:02 +0100

    Passed tests for truncated SVD (only based on desired shape)

--- resolvent_modes.py
+++ resolvent_modes.py
@@ -6,7 +6,7 @@ from traj_util import list2array, array2list
 from Trajectory import Trajectory
 from trajectory_functions import transpose, conj
 
-def resolvent_modes_full(res_inv, cut = 0):
+def resolvent_modes(res_inv, cut = 0):
     # perform full svd
     phi, sig, psi = np.linalg.svd(list2array(res_inv.mode_list))
 
@@ -16,49 +16,24 @@ def resolvent_modes_full(res_inv, cut = 0):
 
     # take inverse of singular values and diagonalise
     sig[1:] = 1.0/sig[1:]
-    sig = [np.diag(sig[i, :]) for i in range(np.shape(sig)[0])]
 
-    # convert to instances of trajectory
-    sig = Trajectory(sig)
-    psi = Trajectory(array2list(psi))
-    phi = Trajectory(array2list(phi))
+    # diagonalize singular value matrix and convert all to lists
+    sig = [np.diag(sig[i, :]) for i in range(res_inv.shape[0])]
+    psi = array2list(psi)
+    phi = array2list(phi)
 
     # loop over swapping rows and columns as necessary
-    for i in range(res_inv.shape[0]):
+    for i in range(1, res_inv.shape[0]):
         sig[i] = np.flip(sig[i])
-        psi[i] = np.flip(psi[i], axis = 0)
+        psi[i] = np.transpose(np.conj(np.flip(psi[i], axis = 0)))
         phi[i] = np.flip(phi[i], axis = 1)
-
-    # take conjugate transpose of left singular matrix (psi)
-    psi = transpose(conj(psi))
 
     # cut off the desired number of singular values
     if cut != 0:
         for i in range(res_inv.shape[0]):
-            sig[i] = sig[i, :-cut, :-cut]
-            psi[i] = psi[i, :, :-cut]
-            phi[i] = phi[i, :, :-cut]
+            sig[i] = sig[i][:-cut, :-cut]
+            psi[i] = psi[i][:, :-cut]
+            phi[i] = phi[i][:, :-cut]
 
-    return phi, sig, psi
-
-if __name__ == '__main__':
-    import random as rand
-    from residual_functions import resolvent_inv
-    from systems import lorenz
-
-    mean = [0, 0, 0]
-    jac_at_mean = lorenz.jacobian(mean)
-
-    no_modes = rand.randint(2, 50)
-    res_inv = resolvent_inv(no_modes, 1, jac_at_mean)
-
-    phi, sig, psi = resolvent_modes_full(res_inv, cut = 0)
-
-    # for i in range(1, no_modes):
-    #     if not np.allclose(np.linalg.inv(res_inv[i]), psi[i] @ sig[i] @ np.transpose(np.conj(phi[i]))):
-    #         print(False)
-
-    a = 1
-    print(sig[a])
-    print(psi[a])
-    print(phi[a])
+    # initialise as trajectory instances and return
+    return Trajectory(psi), Trajectory(sig), Trajectory(phi)
--- unit_tests/TestResolventModes.py
+++ unit_tests/TestResolventModes.py
@@ -8,25 +8,31 @@ import random as rand
 import numpy as np
 from Trajectory import Trajectory
 from trajectory_functions import transpose, conj
-from resolvent_modes import resolvent_modes_full, resolvent_modes_reduced
+from resolvent_modes import resolvent_modes
 
 class TestResolventModes(unittest.TestCase):
 
     def setUp(self):
         self.no_modes = rand.randint(2, 50)
-        dim = rand.randint(2, 5)
+        self.dim = rand.randint(2, 5)
         array = [None]*self.no_modes
         for i in range(self.no_modes):
-            array[i] = np.random.rand(dim, dim) + 1j*np.random.rand(dim, dim)
+            array[i] = np.random.rand(self.dim, self.dim) + 1j*np.random.rand(self.dim, self.dim)
         self.array = Trajectory(array)
 
     def tearDown(self):
         del self.no_modes
+        del self.dim
         del self.array
 
     def test_resolvent_modes_full(self):
         # perform decomposition
-        phi, sig, psi = resolvent_modes_full(self.array)
+        psi, sig, phi = resolvent_modes(self.array)
+
+        # check singular values are in correct order
+        for i in range(self.no_modes):
+            for j in range(self.dim - 1):
+                self.assertTrue(sig[i, j, j] >= sig[i, j + 1, j + 1])
 
         # take inverse of array at each mode
         array_inv_true = [None]*self.no_modes
@@ -43,9 +49,16 @@ class TestResolventModes(unittest.TestCase):
         
         # is zero mode from reconstruction zero matrix
         self.assertTrue(np.array_equal(np.zeros_like(self.array[0]), array_inv[0]))
+    
+    def test_resolvent_modes_truncated(self):
+        # perform truncated svd
+        cut = rand.randint(0, self.dim - 1)
+        psi, sig, phi = resolvent_modes(self.array, cut = cut)
 
-    def test_resolvent_modes_reduced(self):
-        pass
+        # check correct size
+        self.assertEqual(psi.shape, (self.no_modes, self.dim, self.dim - cut))
+        self.assertEqual(sig.shape, (self.no_modes, self.dim - cut, self.dim - cut))
+        self.assertEqual(phi.shape, (self.no_modes, self.dim, self.dim - cut))
 
 
 if __name__ == '__main__':
