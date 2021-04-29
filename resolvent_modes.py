4243306d62bc13aad012f3354c1c2badec8306d7
Thomas Burton <tburton5572@gmail.com>
2021-04-29 11:37:46 +0100

    Implemented truncation to SVD of resolvent singular modes (needs testing)

--- resolvent_modes.py
+++ resolvent_modes.py
@@ -6,36 +6,40 @@ from traj_util import list2array, array2list
 from Trajectory import Trajectory
 from trajectory_functions import transpose, conj
 
-def resolvent_modes_full(res_inv):
+def resolvent_modes_full(res_inv, cut = 0):
     # perform full svd
     phi, sig, psi = np.linalg.svd(list2array(res_inv.mode_list))
 
     # set first element of phi and psi to zero matrices
-    phi[0] = np.zeros_like(phi[0])
     psi[0] = np.zeros_like(psi[0])
+    phi[0] = np.zeros_like(phi[0])
 
     # take inverse of singular values and diagonalise
     sig[1:] = 1.0/sig[1:]
     sig = [np.diag(sig[i, :]) for i in range(np.shape(sig)[0])]
 
     # convert to instances of trajectory
-    phi = Trajectory(array2list(phi))
     sig = Trajectory(sig)
     psi = Trajectory(array2list(psi))
+    phi = Trajectory(array2list(phi))
 
     # loop over swapping rows and columns as necessary
     for i in range(res_inv.shape[0]):
-        phi[i] = np.flip(phi[i], axis = 1)
         sig[i] = np.flip(sig[i])
         psi[i] = np.flip(psi[i], axis = 0)
+        phi[i] = np.flip(phi[i], axis = 1)
 
     # take conjugate transpose of left singular matrix (psi)
     psi = transpose(conj(psi))
 
-    return phi, sig, psi
+    # cut off the desired number of singular values
+    if cut != 0:
+        for i in range(res_inv.shape[0]):
+            sig[i] = sig[i, :-cut, :-cut]
+            psi[i] = psi[i, :, :-cut]
+            phi[i] = phi[i, :, :-cut]
 
-def resolvent_modes_reduced(resolvent):
-    pass
+    return phi, sig, psi
 
 if __name__ == '__main__':
     import random as rand
@@ -48,8 +52,13 @@ if __name__ == '__main__':
     no_modes = rand.randint(2, 50)
     res_inv = resolvent_inv(no_modes, 1, jac_at_mean)
 
-    phi, sig, psi = resolvent_modes_full(res_inv)
+    phi, sig, psi = resolvent_modes_full(res_inv, cut = 0)
 
-    for i in range(1, no_modes):
-        if not np.allclose(np.linalg.inv(res_inv[i]), psi[i] @ sig[i] @ np.transpose(np.conj(phi[i]))):
-            print(False)
+    # for i in range(1, no_modes):
+    #     if not np.allclose(np.linalg.inv(res_inv[i]), psi[i] @ sig[i] @ np.transpose(np.conj(phi[i]))):
+    #         print(False)
+
+    a = 1
+    print(sig[a])
+    print(psi[a])
+    print(phi[a])
