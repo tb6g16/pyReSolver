1399de0ebcf7da12990b9c5d5e55d5b0b67212f1
Thomas Burton <tburton5572@gmail.com>
2021-10-12 13:42:38 +0100

    Changed setup.py to allow editable install of package

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-from distutils.core import setup, Extension
+from setuptools import setup, find_packages
+
+# THIS STUFF HAS TO IMPLEMENTED FOR THE C EXTENSIONS
+# def main():
+#     setup(name = "ctrajmul",
+#           description = "Python interface for trajmul C function",
+#           author = "Thomas Burton",
+#           author_email = "tb6g16@soton.ac.uk",
+#           ext_modules = [Extension("ctrajmul", ["./ResolventSolver/csrc/ctrajmul.c"])])
 
 def main():
-    setup(name = "ctrajmul",
-          description = "Python interface for trajmul C function",
-          author = "Thomas Burton",
-          author_email = "tb6g16@soton.ac.uk",
-          ext_modules = [Extension("ctrajmul", ["./ResolventSolver/csrc/ctrajmul.c"])])
+    setup(
+        name = 'ResolventSolver',
+        packages = find_packages(include = ['ResolventSolver', 'ResolventSolver.*']),
+        install_requires = ['numpy',
+            'scipy',
+            'matplotlib',
+            'h5py',
+            'snakeviz']
+    )
 
 # ext_package = 'ResolventSolver'
 
