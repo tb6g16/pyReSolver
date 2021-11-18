046ce31aa8680f63f330e1d24e233a21d2291fac
Thomas Burton <tburton5572@gmail.com>
2021-11-18 11:13:48 +0000

    removed unecessary C source files (found numpy alternative to desired extension functions)

--- setup.py
+++ setup.py
@@ -1,26 +1,15 @@
 from setuptools import setup, find_packages
 
-# THIS STUFF HAS TO IMPLEMENTED FOR THE C EXTENSIONS
-# module = Extension('demo', include_dirs = ['some include dirs'], sources = ['demo.c'])
-# def main():
-#     setup(name = "ctrajmul",
-#           description = "Python interface for trajmul C function",
-#           author = "Thomas Burton",
-#           author_email = "tb6g16@soton.ac.uk",
-#           ext_modules = [Extension("ctrajmul", ["./ResolventSolver/csrc/ctrajmul.c"])])
-
 def main():
     setup(
         name = 'ResolventSolver',
         packages = find_packages(include = ['ResolventSolver', 'ResolventSolver.*']),
         install_requires = ['numpy',
-            'scipy',
-            'matplotlib',
-            'h5py',
-            'snakeviz']
+                            'scipy',
+                            'matplotlib',
+                            'h5py',
+                            'snakeviz']
     )
-
-# ext_package = 'ResolventSolver'
 
 if __name__ == '__main__':
     main()
