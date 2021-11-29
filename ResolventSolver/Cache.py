c44dfc7e68897d15c90f275bec0c3a019d597895
Thomas Burton <tburton5572@gmail.com>
2021-11-29 12:18:05 +0000

    defined cache file to be implemented at some other time

---
+++ ResolventSolver/Cache.py
@@ -0,0 +1,8 @@
+# This file contains the definition for a class that holds a cache of all the
+# objects that are required at each iteration of the optimisation procedure.
+# This is done for the purpose of organising exactly what arrays have been
+# allocated in memory.
+
+# To get this going, I need to list ALL of the arrays I need to complete a
+# single iteration of the optimisation loop. These arrays are re-used over each
+# iteration.
---
+++ ResolventSolver/tests/TestCache.py
@@ -0,0 +1,15 @@
+# This file contains the tests for the Cache class and it's methods.
+
+import unittest
+
+class TestCache(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
