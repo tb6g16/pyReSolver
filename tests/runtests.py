abadd4b12d94614d478f6adde11e433d8c36807c
Thomas Burton <tburton5572@gmail.com>
2024-01-25 14:08:54 +0000

    got rid of __init__.py from test directory since it is no longer in the package

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,15 +1,15 @@
 import unittest
 
-from tests.TestFFTPlans import TestFFTPlans
-# from pyReSolver.tests.TestHessian import TestHessian
-from tests.TestInitOptFuncs import TestInitOptFuncs
-from tests.TestResidualFunctions import TestResidualFunctions
-from tests.TestResolventModes import TestResolventModes
-from tests.TestSparseLinalgEigs import TestSparseLinalgEigs
-from tests.TestTraj2Vec import TestTraj2Vec
-from tests.TestTrajectoryFunctions import TestTrajectoryFunctions
-from tests.TestTrajectoryMethods import TestTrajectoryMethods
-from tests.TestTrajUtil import TestTrajUtil
+from TestFFTPlans import TestFFTPlans
+# from TestHessian import TestHessian
+from TestInitOptFuncs import TestInitOptFuncs
+from TestResidualFunctions import TestResidualFunctions
+from TestResolventModes import TestResolventModes
+from TestSparseLinalgEigs import TestSparseLinalgEigs
+from TestTraj2Vec import TestTraj2Vec
+from TestTrajectoryFunctions import TestTrajectoryFunctions
+from TestTrajectoryMethods import TestTrajectoryMethods
+from TestTrajUtil import TestTrajUtil
 
 if __name__ == '__main__':
     unittest.main()
